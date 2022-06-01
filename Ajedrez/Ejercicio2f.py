bdb862cf40bcef13037dff14dc9b0ff94784e871
Triplerush <fcanal@unsa.edu.pe>
2022-06-01 12:08:04 -0500

    Ejercicio f terminado

--- Ajedrez/Ejercicio2f.py
+++ Ajedrez/Ejercicio2f.py
@@ -1,2 +1,12 @@
 from interpreter import draw
 from chessPictures import *
+
+picture = square.join(square.negative())
+
+picture = picture.horizontalRepeat(4)
+
+picture = picture.negative().up(picture)
+
+picture = picture.verticalRepeat(2)
+
+draw(picture)
