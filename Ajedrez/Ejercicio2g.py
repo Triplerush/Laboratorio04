463e960fdd018425fa4f803ba90cdc0ef0a87137
Triplerush <fcanal@unsa.edu.pe>
2022-06-01 12:55:41 -0500

    Ejercicio g terminado

--- Ajedrez/Ejercicio2g.py
+++ Ajedrez/Ejercicio2g.py
@@ -1,2 +1,22 @@
 from interpreter import draw
 from chessPictures import *
+
+#Piezas
+piecesWhite = rock.join(knight).join(bishop)
+piecesWhite = piecesWhite.join(queen).join(king).join(piecesWhite)
+piecesBlack = piecesWhite.negative()
+piecesWhite = piecesWhite.up(pawn.horizontalRepeat(8))
+piecesBlack = pawn.horizontalRepeat(8).negative().up(piecesBlack)
+
+#Tablero
+picture = square.join(square.negative())
+picture = picture.horizontalRepeat(4)
+picture = picture.negative().up(picture)
+
+whiteSelf = picture.under(piecesWhite)
+blackSelf = picture.under(piecesBlack)
+center = picture.verticalRepeat(2)
+
+tablero = whiteSelf.up(center).up(blackSelf)
+
+draw(tablero)
